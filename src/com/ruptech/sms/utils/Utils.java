6ea8fafb9dbd180251f592818df564d643011dc9
liujiuyi <296096828@qq.com>
2016-01-25 19:14:10 +0800

    ctalk4

--- src/com/ruptech/sms/utils/Utils.java
+++ src/com/ruptech/sms/utils/Utils.java
@@ -2,7 +2,7 @@ package com.ruptech.sms.utils;
 
 public class Utils {
 	public static final String CATEGORY = "sms.";
-	public static final String SERVER_URL = "http://120.27.138.93/tttalk151218";
+	public static final String SERVER_URL = "http://ctalk4/tttalk151218";
 
 	public static boolean isEmpty(String s) {
 		return s == null || s.length() == 0 || s.equals("null");
